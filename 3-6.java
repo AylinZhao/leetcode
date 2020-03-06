3ad6bee479bd291c063ad1ab97832ea38e828721
aylinzhao <aylinzhao@qq.com>
2020-03-06 16:57:44 +0800

    train

---
+++ 3-6.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void merge(int[] A, int m, int[] B, int n) {
+    	if (B.length == 0 || A.length == 0) return;
+    	while (m >= 0 && n >= 0) {
+            if (m == 0 && n != 0) {
+                A[n - 1] = B[n - 1];
+                n--;
+                continue;
+            }
+            if (n == 0) return;
+    		if (A[m - 1] >= B[n - 1]) {
+    			A[m + n - 1] = A[m - 1];
+    			m--;
+    		} else {
+    			A[m + n - 1] = B[n - 1];
+    			n--;
+    		}
+    	}
+    }
+}
