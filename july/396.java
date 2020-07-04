e75f3e6dfe864e3b89799c0eab669c33ddcf938c
Aylin Zhao <aylinzhao@qq.com>
2020-07-04 17:19:46 +0800

    add code

--- july/396.java
+++ july/396.java
@@ -1,23 +1,22 @@
 class Solution {
     public int maxRotateFunction(int[] A) {
         if (A == null || A.length == 0) return 0;
-    	int n = A.length;
-    	int max = Integer.MIN_VALUE;
-    	for (int st = 0; st < n; st++) {
-    		max = Math.max(count(A, st), max);
-    	}
-    	return max;
-    }
-    public int count(int[] A, int st) {
-    	int len = A.length;
-    	int res = 0;
-    	int i = 0;
-        // int start = st;
-    	while (i < len) {
-    		res += A[st % len] * i;
-    		i++;
-    		st++; 
-    	}
-    	return res;
+        int N = A.length;
+        int base = 0;
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += A[i];
+            base += A[i] * i;
+        }
+        int max = base;
+        int current = 0;
+        // System.out.println("current max value : " + base);
+        for (int j = N - 1; j > 0; j--) {
+            current =  base + (sum - A[j] * N);
+            // System.out.println("current max value : " +current);
+            max = Math.max(current, max);
+            base = current;
+        }
+        return max;
     }
 }
