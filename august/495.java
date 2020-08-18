867b650bc673034d529194704d0098a3ecd68c3b
ning zhao <1491447203@qq.com>
2020-08-18 22:22:29 +0800

    add code

--- august/495.java
+++ august/495.java
@@ -1,5 +1,18 @@
 class Solution {
     public int findPoisonedDuration(int[] timeSeries, int duration) {
-
+    	int l = 0;
+    	int r = 0;
+    	int res = 0;
+    	for (int i = 0; i < timeSeries.length; i++) {
+    		if (r <= timeSeries[i]) {
+    			res += r - l;
+    		} else if (r > timeSeries[i] && timeSeries[i] > l) {
+    			res += timeSeries[i] - l;
+    		}
+    		l = timeSeries[i];
+			r = timeSeries[i] + duration;
+    	}
+    	res += (r - l);
+    	return res;
     }
 }
