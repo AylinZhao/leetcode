4dc6818f3b28cf3cb186fecdcb9166fe4727bbb7
aylinzhao <aylinzhao@qq.com>
2020-08-21 16:59:58 +0800

    add code

---
+++ august/498.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] findDiagonalOrder(int[][] matrix) {
+    	ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+    	for (int i = 0; i < matrix[0].length; i++) {
+    		ArrayList<Integer> al = new ArrayList<Integer>();
+    		int p = 0;
+    		int q = i;
+    		while ((++p) < matrix.length && (--q) >= 0) {
+    			
+    		}
+    	}
+
+    }
+}
