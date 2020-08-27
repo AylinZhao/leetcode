8d6dfa0b6fdbd4d19737c49581acfb16df2639ae
AylinZhao <1491447203@qq.com>
2020-08-27 23:33:26 +0800

    Update 498.java

--- august/498.java
+++ august/498.java
@@ -1,14 +1,47 @@
 class Solution {
     public int[] findDiagonalOrder(int[][] matrix) {
-    	ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
-    	for (int i = 0; i < matrix[0].length; i++) {
-    		ArrayList<Integer> al = new ArrayList<Integer>();
-    		int p = 0;
-    		int q = i;
-    		while ((++p) < matrix.length && (--q) >= 0) {
-    			
-    		}
-    	}
-
+        if (matrix  == null || matrix.length == 0) return new int[0];
+        ArrayList<ArrayList<Integer>> record = new ArrayList<>();
+        int sum = 0;
+        for (int i = 0; i < matrix[0].length; i++) {
+            int row = 0;
+            int col = i;
+            ArrayList<Integer> l = new ArrayList<>();
+            l.add(matrix[row][col]);
+            while ((++row) < matrix.length && (--col) >= 0) {
+                l.add(matrix[row][col]);
+            }
+            sum += l.size();
+            record.add(l);
+        }
+        for (int j = 1; j < matrix.length; j++) {
+            int row = j;
+            int col = matrix[0].length - 1;
+            ArrayList<Integer> l = new ArrayList<>();
+            l.add(matrix[row][col]);
+            while ((++row) < matrix.length && (--col) >= 0) {
+                l.add(matrix[row][col]);
+            }
+            sum += l.size();
+            record.add(l);
+        }
+        int[] res = new int[sum];
+        boolean f = false;
+        int cur = 0;
+        for (int i = 0; i < record.size(); i++) {
+            ArrayList<Integer> ele = record.get(i);
+            if (f) {
+                for (int j = 0; j < ele.size();) {
+                    res[cur++] = ele.get(j++);
+                }
+                f = false;
+            } else if (!f) {
+                for (int j = ele.size() - 1; j >= 0;) {
+                    res[cur++] = ele.get(j--);
+                }
+                f = true;
+            }
+        }
+        return res;
     }
 }
