ba80efa92292a8b427ff9180a20752845b81f14a
AylinZhao <1491447203@qq.com>
2020-11-11 23:12:11 +0800

    Create 21.java

---
+++ other/21.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        ListNode head;
+        if (l1.val <= l2.val) {
+            head = l1;
+            l1 = l1.next;
+        } else {
+            head = l2;
+            l2 = l2.next;
+        }
+        ListNode p = l1;
+        ListNode q = l2;
+        ListNode k = head;
+        while(p != null && q != null) {
+            if (p.val <= q.val) {
+                k.next = p;
+                p = p.next;
+            } else {
+                k.next = q;
+                q = q.next;
+            }
+            k = k.next;
+        }
+        if (p == null || q != null) k.next = q;    
+        if (q == null || p != null) k.next = p;    
+        return head;
+    }
+}
